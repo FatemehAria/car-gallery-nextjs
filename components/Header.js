97a65821d843f7f6229732ea5eb7d0bc31c12ec7
FatemehAria <ftemewh@gmail.com>
2023-11-16 20:27:20 +0330

    header set

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+"use client"
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+export default function Header() {
+    const router = useRouter()
+  return (
+    <div className="flex justify-evenly items-center p-5">
+      <button onClick={() => router.push("/login")}>Login</button>
+      <input placeholder="search your car" />
+      <Image src="/favicon.ico" width={40} height={40} alt="logo"/>
+    </div>
+  );
+}
