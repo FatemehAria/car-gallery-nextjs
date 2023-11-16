5e62c7beac1b6cc4a378d1e34e0da40fad66be98
FatemehAria <ftemewh@gmail.com>
2023-11-16 22:06:05 +0330

    categories component created

---
+++ components/Catgories.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import styles from "./categories.module.css";
+import Hatchback from "@/public/icons/Hatchback";
+import Sedan from "@/public/icons/Sedan";
+import Sport from "@/public/icons/Sport";
+import Suv from "@/public/icons/Suv";
+
+const Categories = () => {
+  return (
+    <div
+      className={`flex justify-center items-center gap-4 font-semibold ${styles.aTagsContainer}`}
+    >
+      <Link href="/sedan">
+        Sedan
+        <Sedan />
+      </Link>
+      <Link href="/suv">
+        SUV
+        <Suv />
+      </Link>
+      <Link href="/hatchback">
+        Hatchback
+        <Hatchback />
+      </Link>
+      <Link href="/sport">
+        Sport
+        <Sport />
+      </Link>
+    </div>
+  );
+};
+export default Categories;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-import Hatchback from "@/public/icons/Hatchback";
-import Sedan from "@/public/icons/Sedan";
-import Sport from "@/public/icons/Sport";
-import Suv from "@/public/icons/Suv";
+import Categories from "@/components/Catgories";
 import Head from "next/head";
-import Link from "next/link";
-import styles from "./Home.module.css";
 
 export default function Home() {
   return (
@@ -17,24 +12,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <div className={`flex justify-center items-center gap-4 font-semibold ${styles.aTagsContainer}`}>
-          <Link href="/sedan">
-            Sedan
-            <Sedan />
-          </Link>
-          <Link href="/suv">
-            SUV
-            <Suv />
-          </Link>
-          <Link href="/hatchback">
-            Hatchback
-            <Hatchback />
-          </Link>
-          <Link href="/sport">
-            Sport
-            <Sport />
-          </Link>
-        </div>
+        <Categories />
       </main>
     </>
   );
