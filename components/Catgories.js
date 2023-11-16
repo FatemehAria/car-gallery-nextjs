833a43e9e8f0ee381a15c00e91840e98a9c98646
FatemehAria <ftemewh@gmail.com>
2023-11-16 22:35:36 +0330

    back arrow added to category page

--- components/Catgories.js
+++ components/Catgories.js
@@ -4,29 +4,32 @@ import Hatchback from "@/public/icons/Hatchback";
 import Sedan from "@/public/icons/Sedan";
 import Sport from "@/public/icons/Sport";
 import Suv from "@/public/icons/Suv";
+import React from "react";
 
 const Categories = () => {
   return (
-    <div
-      className={`flex justify-center items-center gap-4 font-semibold ${styles.aTagsContainer}`}
-    >
-      <Link href="/sedan">
-        Sedan
-        <Sedan />
-      </Link>
-      <Link href="/suv">
-        SUV
-        <Suv />
-      </Link>
-      <Link href="/hatchback">
-        Hatchback
-        <Hatchback />
-      </Link>
-      <Link href="/sport">
-        Sport
-        <Sport />
-      </Link>
-    </div>
+    <React.Fragment>
+      <div
+        className={`flex justify-center items-center gap-4 font-semibold ${styles.aTagsContainer}`}
+      >
+        <Link href="/sedan">
+          Sedan
+          <Sedan />
+        </Link>
+        <Link href="/suv">
+          SUV
+          <Suv />
+        </Link>
+        <Link href="/hatchback">
+          Hatchback
+          <Hatchback />
+        </Link>
+        <Link href="/sport">
+          Sport
+          <Sport />
+        </Link>
+      </div>
+    </React.Fragment>
   );
 };
 export default Categories;
--- pages/[categories].js
+++ pages/[categories].js
@@ -1,16 +1,20 @@
+"use client"
 import { useRouter } from "next/router";
 import data from "../data/carsData";
 import CarCard from "@/components/CarCard";
 import React from "react";
+import { MdArrowBack } from "react-icons/md";
 
 export default function Category() {
   const router = useRouter();
   const { categories } = router.query;
-  return (
-    <div>
-      {data.map((item) => item.category === categories && <React.Fragment key={item.id}>
-        <CarCard car={...item}/>
-      </React.Fragment>)}
+  return <div>
+    <MdArrowBack className="text-2xl font-semibold m-2" onClick={() => router.push("/")}/>
+      {
+        data.map((item) => item.category === categories && <React.Fragment key={item.id}>
+        <CarCard car= {...item} />
+      </React.Fragment>)
+      }
     </div>
-  );
+  ;
 }
