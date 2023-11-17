9bb6d3d1162d0705a0869bbbc3fb58e9c1a6d256
FatemehAria <ftemewh@gmail.com>
2023-11-17 16:00:22 +0330

    car info styling initialized

--- components/CarData.js
+++ components/CarData.js
@@ -4,37 +4,54 @@ import Location from "@/public/icons/Location";
 import Model from "@/public/icons/Model";
 import Road from "@/public/icons/Road";
 import Image from "next/image";
-
+import styles from "./cardata.module.css";
 export default function CarData({ item }) {
   // console.log(item);
   return (
-    <div>
+    <div className="flex flex-col">
       <Image
         width={500}
         height={500}
         src={`${item.image}`}
-        alt={`${item.name + item.model}`}
+        alt={`${item.name} ${item.model}`}
       />
-      <p>
-        ${item.name} ${item.model}
+      <p className="font-bold text-xl p-2">
+        {item.name} {item.model}
       </p>
-      <div>
-        <Company />
-        <span>Company</span>
-        <Model />
-        <span>Model</span>
-        <Calender />
-        <span>First registration</span>
-        <Road />
-        <span>Kms driven</span>
+
+      <div className={`${styles.overallInfo}`}>
+        <div>
+          <Company />
+          <p>Company</p>
+        </div>
+        <div>
+          <Model />
+          <p>
+            Model <span>{item.model}</span>
+          </p>
+        </div>
+        <div>
+          <Calender />
+          <p>First registration</p>
+        </div>
+        <div>
+          <Road />
+          <p>
+            Kms driven <span>{item.distance}km</span>
+          </p>
+        </div>
       </div>
-      <div>
-        <Location />
-        <span>Location</span>
+
+      <div className={`${styles.overallInfo}`}>
+        <div>
+          <Location />
+          <p>Location <span>{item.location}</span></p>
+        </div>
       </div>
+
       <div>
-        <p>Extra Information</p>
-        <p>${item.description}</p>
+        <p className="font-bold">Extra Information</p>
+        <p className="p-2 text-justify">{item.description}</p>
       </div>
     </div>
   );
