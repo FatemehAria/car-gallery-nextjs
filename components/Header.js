fbe0fe2a8242b1076b1d7abde16c390710bc892b
FatemehAria <ftemewh@gmail.com>
2023-11-17 18:57:32 +0330

    login btn added, page is left

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,7 @@ import data from "../data/carsData";
 import { useState } from "react";
 import CarCard from "./CarCard";
 import Link from "next/link";
+import { BiSolidLogInCircle } from "react-icons/bi";
 
 export default function Header() {
   const router = useRouter();
@@ -12,15 +13,15 @@ export default function Header() {
   const [fade, setFade] = useState(false);
 
   return (
-    <div className="flex justify-evenly items-center p-5">
-      <button onClick={() => router.push("/login")}>Login</button>
+    <div className="flex justify-evenly items-center p-5 w-[80%] mx-auto">
+      <BiSolidLogInCircle onClick={() => router.push("/login")} className="text-3xl font-semibold text-[#B0578D] cursor-pointer"/>
 
       <div className="flex flex-col w-full justify-center items-center">
         <input
           placeholder="search your car"
           value={text}
           onChange={(e) => setText(e.target.value)}
-          className="outline-none rounded-md border p-1 border-gray-800 w-[40%]"
+          className="outline-none rounded-md border p-2 border-gray-800 w-[40%]"
         />
         <div className="absolute bg-[#F4EAE0] top-[12%] mx-4 rounded-lg md:flex md:w-[75%] md:justify-center md:items-center lg:w-[50%]">
           {data.map(
